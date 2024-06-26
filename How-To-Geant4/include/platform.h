cbb2f3fb80120a822f835bb83f53fb38a0cfbec8
Leoni <leoni.dessau@mailbox.org>
2024-06-26 13:54:27 +0200

    added platform.h to handle unix and apple specific imports

--- How-To-Geant4/include/platform.h
+++ How-To-Geant4/include/platform.h
@@ -1,3 +1,7 @@
+/*
+these includes need to be present in all files that use
+_getpid() so that compilation on macos works (and supposedly unix)
+*/
 #if __unix__ || __APPLE__                              // for checking if the code shall be compiled on an UNIX system
 #include <unistd.h>                       //To use getpid() to get the process ID to use as random seed on UNIX systems
 #include <sys/types.h>                    //To use getpid() to get the process ID to use as random seed on UNIX systems
