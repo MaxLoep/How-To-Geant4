b482ba73ce011479f77cb3e043aa6dda2412c338
Leoni <leoni.dessau@mailbox.org>
2025-03-14 15:32:04 +0100

    we has collimator

--- How-To-Geant4/src/API_utils.cc
+++ How-To-Geant4/src/API_utils.cc
@@ -74,10 +74,15 @@ void api::add_placer(std::string name, placer_func func) {
 	geometries::add_placer(name, func);
 }
 
+#include "Collimator.cc"
 
 void api::setup_sim() {
 	//place_geometry("SD1", "cube", {{"x_pos", 0.}, {"y_pos", 0.}, {"z_pos", 10.*cm}, {"x_size", 15. * cm}, {"y_size", 15. * cm}, {"z_size", 0.02 * mm}}, Materials::Vacuum);
 	//make_sd("SD1", "neutron", {property::Ekin, property::time});
+
+	add_placer("collimator", collimator);
+
+	place_geometry("THEcollimator", "collimator", {{"x_pos", 2. * cm}, {"y_pos", 0.}, {"z_pos", 0.*cm}}, Materials::Vacuum);
 
 	place_geometry(
 		"PIXE", "sphere",
