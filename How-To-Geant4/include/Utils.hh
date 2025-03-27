f0060fdb69ae8bb18bd4524334391eebdb8cfc52
MaxLoep <loepke@uni-bonn.de>
2025-03-27 16:18:48 +0100

    EITHER and OPTIONS are back

--- How-To-Geant4/include/Utils.hh
+++ How-To-Geant4/include/Utils.hh
@@ -37,42 +37,42 @@ struct v3 {
 
 };
 
-// template <typename T> struct Option{
-// 	bool some = false;
-// 	T inner;
-// 	Option<T> (){
-// 		this->some = false;
-// 	};
+template <typename T> struct Option{
+	bool some = false;
+	T inner;
+	Option<T> (){
+		this->some = false;
+	};
 
-// 	Option<T> (T elem) {
-// 		this->some = true;
-// 		this->inner = elem;
-// 	};
-// };
+	Option<T> (T elem) {
+		this->some = true;
+		this->inner = elem;
+	};
+};
 
-// template <typename A, typename B>
-// struct Either{
-// 	bool has_a;
-// 	A option_a;
-// 	B option_b;
-// 	Either<A, B> (A elem) {
-// 		this->option_a = elem;
-// 		this->has_a = true;
-// 	}
+template <typename A, typename B>
+struct Either{
+	bool has_a;
+	A option_a;
+	B option_b;
+	Either<A, B> (A elem) {
+		this->option_a = elem;
+		this->has_a = true;
+	}
 
-// 	Either<A, B> (B elem) {
-// 		this->option_b = elem;
-// 		this->has_a = false;
-// 	}
+	Either<A, B> (B elem) {
+		this->option_b = elem;
+		this->has_a = false;
+	}
 
-// 	Option<A> get_A() {
-// 		return this->has_a? Option<A>(this->option_a): Option<A>();
-// 	};
+	Option<A> get_A() {
+		return this->has_a? Option<A>(this->option_a): Option<A>();
+	};
 
-// 	Option<A> get_B() {
-// 		return !this->has_a? Option<B>(this->option_b): Option<B>();
-// 	};
-// };
+	Option<A> get_B() {
+		return !this->has_a? Option<B>(this->option_b): Option<B>();
+	};
+};
 
 }
 #endif
