553d4e4d8f9f151fc52dcd7d13611082dd082185
MaxLoep <loepke@uni-bonn.de>
2025-07-14 15:30:15 +0200

    macro switchero for LE-P+A

--- How-To-Geant4/src/API_utils.cc
+++ How-To-Geant4/src/API_utils.cc
@@ -117,20 +117,20 @@ void api::setup_sim() {
 
 
 	// SANDWICH GEOMETRY START
-	G4int layers = 5;
-	// G4double width = 0.0225*mm;
-	G4double width = 10.*cm;
-	G4String particle = "proton";
+	G4int layers = 1;
+	G4double width = 0.0225*mm;
+	// G4double width = 10.*cm;
+	G4String particle = "alpha";
 
 	for (int i = 0; i < layers; i++) {
-		place_geometry(//Plane0
+		place_geometry(//SD-Plane
 			"Plane_" + std::to_string(i), "cube",
 			{{"x_pos", 0.}, {"y_pos", 0.}, {"z_pos", (width+0.1*mm)*i}, {"rot_x", 0. * degree}, {"x_size", 50. * cm}, {"y_size", 50. * cm}, {"z_size", 0.1*mm/2}},
 			Materials::Vacuum
 		);
 		make_sd("Plane_" + std::to_string(i), particle, {property::Ekin, property::local_pos_x, property::local_pos_y});
 		
-		place_geometry(//Air1
+		place_geometry(//Material
 			"Air_" + std::to_string(i), "cube",
 			{{"x_pos", 0.}, {"y_pos", 0.}, {"z_pos", (width+0.1*mm)/2 + (width+0.1*mm)*i}, {"rot_x", 0. * degree}, {"x_size", 50. * cm}, {"y_size", 50. * cm}, {"z_size", width/2}},
 			Materials::Aluminum
