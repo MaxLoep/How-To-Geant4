d5423b0a86b8a3f3a2113ac8849cec1aca4a20cf
Max Loep <Loepke@uni-bonn.de>
2026-03-08 00:59:32 +0100

    Generic SD now samples Half life time instead of mean life time

--- How-To-Geant4/src/GenericSD.cc
+++ How-To-Geant4/src/GenericSD.cc
@@ -88,7 +88,7 @@ G4bool GenericSD::ProcessHits(G4Step* step, G4TouchableHistory* /*history*/) {
 	// }
 
 	const G4ParticleDefinition* particle = track->GetParticleDefinition();
-	G4double life_time = particle->GetPDGLifeTime();
+	G4double life_time = particle->GetPDGLifeTime() / 1.443;
 	// if particle is a secondary (trackID>1) and we have not counted it yet add it to the map
 	if ( (currentTrackId > 1) && (currentTrackId != this->oldTrackId) ) {
 		//G4cout << this->name <<  " detected: " << particle_name << this->particle_map[particle_name] + 1  << "times" << G4endl;
